c9b3d347d21abb3088a63475008e45dccc9d75ca
AlessioAm <alejandrafernandez@Alessios-Mac.local>
2014-07-24 00:48:29 -0300

    Fixed Python Format
    I wanted to try something else but my IDE was bothering me about
    formatting. Best formatting is a consistent format between spaces and
    indents. Everything is now what my IDE automatically has indented.
    Also, I’ve added some files in “.idea”. Don’t worry about them.

--- main.py
+++ main.py
@@ -9,46 +9,50 @@ from kivy.uix.button import Button
 from kivy.uix.accordion import Accordion, AccordionItem
 from kivy.garden.navigationdrawer import NavigationDrawer
 
-Builder.load_file('main.kv')  #keep main.kv in the same directory
+Builder.load_file('main.kv')   # keep main.kv in the same directory
+
 
 class MyDrop(DropDown):
-	for i in range(5):
-		print i
+    for i in range(5):
+        print i
+
 
 class Notes(Screen, BoxLayout):
-	top_layout = ObjectProperty(None)
-	TextSpace = ObjectProperty(None)
-	quick_settings = ObjectProperty(None)
-	
-	def __init__(self, *args, **kwargs):
-		super(Notes, self).__init__(*args, **kwargs)
-		self.drop_down = MyDrop()
-		
-		dropdown = DropDown()
-		notes = ["Notes", "Sketch", "To-do"]
-		for note in notes:
-			btn = Button(text='%r' % note, size_hint_y=None, height=40)
-		
-			btn.bind(on_release=lambda btn: dropdown.select(btn.text))
-			
-			dropdown.add_widget(btn)
-		
-		mainbutton = Button(text='Notes', size_hint=(1, 1))
-		mainbutton.bind(on_release=dropdown.open)
-		
-		dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
-		
-		self.top_layout.add_widget(mainbutton)
-	
+    top_layout = ObjectProperty(None)
+    TextSpace = ObjectProperty(None)
+    quick_settings = ObjectProperty(None)
+
+    def __init__(self, *args, **kwargs):
+        super(Notes, self).__init__(*args, **kwargs)
+        self.drop_down = MyDrop()
+
+        dropdown = DropDown()
+        notes = ["Notes", "Sketch", "To-do"]
+
+        for note in notes:
+            btn = Button(text='%r' % note, size_hint_y=None, height=40)
+            btn.bind(on_release=lambda btn: dropdown.select(btn.text))
+            dropdown.add_widget(btn)
+
+        mainbutton = Button(text='Notes', size_hint=(1, 1))
+        mainbutton.bind(on_release=dropdown.open)
+
+        dropdown.bind(on_select=lambda instance, x: setattr(mainbutton, 'text', x))
+
+        self.top_layout.add_widget(mainbutton)
+
+
 class SettingsScreen(Screen):
-	pass
-	
+    pass
+
+
 class ToDoScreen(Screen):
-	pass
-	
+    pass
+
+
 class SketchScreen(Screen):
-	pass
-	
+    pass
+
 sm = ScreenManager(transition=SlideTransition(direction='up'))
 
 sm.add_widget(Notes(name='home'))
@@ -57,8 +61,8 @@ sm.add_widget(ToDoScreen(name='todo'))
 sm.add_widget(SketchScreen(name='sketch'))
 
 class MyApp(App):
-	def build(self):
-		return sm
+    def build(self):
+        return sm
 
 if __name__ == '__main__':
-	MyApp().run()
+    MyApp().run()
